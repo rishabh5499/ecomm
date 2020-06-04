d696f0bbe9d26abab34df9fbd98d1255ec60373f
Rishabhbaliga <35658972+Rishabhbaliga@users.noreply.github.com>
2020-06-05 03:11:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.use(productsRouter);
 app.use(adminProductsRouter);
 app.use(cartsRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log('Server Started');
-});
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  console.log("Server has started");
+  });
